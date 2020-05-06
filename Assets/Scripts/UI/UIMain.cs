e9a7b03ccd4b843353200cda2697627025435182
xx <xxly0528@gmail.com>
2020-05-07 00:05:18 +0800

    移动

--- Assets/Scripts/UI/UIMain.cs
+++ Assets/Scripts/UI/UIMain.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 
 public class UIMain : MonoBehaviour
 {
+    [SerializeField]
+    Player player;
+
+    Vector2 centerPos = Vector2.zero;
+
     public Button btnBg;
     public Button btnSetting;
     public Button btnTask;
@@ -52,22 +57,36 @@ public class UIMain : MonoBehaviour
         btnOther.gameObject.SetActive(false);
         btnBg.gameObject.SetActive(false);
         IsStart = true;
+
     }
 
     void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("OnBeginDrag --- ");
+        player.SetMoveing(true);
+        centerPos = eventData.position;
     }
 
 
-    void OnDrag(PointerEventData data)
+    void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag --- ");
+        var curPos = eventData.position;
+        var moveX = curPos.x - centerPos.x;
+        var moveY = curPos.y - centerPos.y;
+        var dirTemp = new Vector2(moveX, moveY);
+        if (dirTemp.sqrMagnitude >= 100)
+        {
+            dirTemp.x = 10 * Mathf.Cos(Mathf.Atan2(moveY, moveX));
+            dirTemp.y = 10 * Mathf.Sin(Mathf.Atan2(moveY, moveX));
+            centerPos = new Vector2(curPos.x - dirTemp.x, curPos.y - dirTemp.y);
+        }
+
+        player.SetDir(dirTemp);
     }
 
     void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("OnEndDrag --- ");
+        player.SetMoveing(false);
+        player.SetDir(Vector2.zero);
     }
 
     void onClickSetting()
